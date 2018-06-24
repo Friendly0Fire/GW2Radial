708d0ab55acc8049102341db55d27478dc886a42
FriendlyFire <jpguertin.cg@gmail.com>
2018-06-23 21:15:05 -0400

    Window focus loss interrupts overlay. Fixes #12.

--- GW2Mounts/main.cpp
+++ GW2Mounts/main.cpp
@@ -252,203 +252,240 @@ void Shutdown();
 extern LRESULT ImGui_ImplDX9_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	struct EventKey
+	if (msg == WM_KILLFOCUS)
 	{
-		uint vk : 31;
-		bool down : 1;
-	};
-
-	std::list<EventKey> eventKeys;
-
-	// Generate our EventKey list for the current message
-	{
-		bool eventDown = false;
-		switch (msg)
-		{
-		case WM_SYSKEYDOWN:
-		case WM_KEYDOWN:
-			eventDown = true;
-		case WM_SYSKEYUP:
-		case WM_KEYUP:
-			if ((msg == WM_SYSKEYDOWN || msg == WM_SYSKEYUP) && wParam != VK_F10)
-			{
-				if (((lParam >> 29) & 1) == 1)
-					eventKeys.push_back({ VK_MENU, true });
-				else
-					eventKeys.push_back({ VK_MENU, false });
-			}
-
-			eventKeys.push_back({ (uint)wParam, eventDown });
-			break;
-
-		case WM_LBUTTONDOWN:
-			eventDown = true;
-		case WM_LBUTTONUP:
-			eventKeys.push_back({ VK_LBUTTON, eventDown });
-			break;
-		case WM_MBUTTONDOWN:
-			eventDown = true;
-		case WM_MBUTTONUP:
-			eventKeys.push_back({ VK_MBUTTON, eventDown });
-			break;
-		case WM_RBUTTONDOWN:
-			eventDown = true;
-		case WM_RBUTTONUP:
-			eventKeys.push_back({ VK_RBUTTON, eventDown });
-			break;
-		case WM_XBUTTONDOWN:
-			eventDown = true;
-		case WM_XBUTTONUP:
-			eventKeys.push_back({ (uint)(GET_XBUTTON_WPARAM(wParam) == XBUTTON1 ? VK_XBUTTON1 : VK_XBUTTON2), eventDown });
-			break;
-		}
+		CurrentMountHovered = MountType::NONE;
+		DisplayMountOverlay = false;
+		DownKeys.clear();
 	}
-
-	// Apply key events now
-	for (const auto& k : eventKeys)
-		if (k.down)
-			DownKeys.insert(k.vk);
-		else
-			DownKeys.erase(k.vk);
-
-	// Detect hovered section of the radial menu, if visible
-	if (DisplayMountOverlay && msg == WM_MOUSEMOVE)
-		DetermineHoveredMount();
-
-	bool isMenuKeybind = false;
-
-	// Only run these for key down/key up (incl. mouse buttons) events
-	if (!eventKeys.empty())
+	else
 	{
-		// Very exclusive test: *only* consider the menu keybind to be activated if they're the *only* keys currently down
-		// This minimizes the likelihood of the menu randomly popping up when it shouldn't
-		isMenuKeybind = DownKeys == Cfg.SettingsKeybind();
-
-		if(isMenuKeybind)
-			DisplayOptionsWindow = true;
-		else
+		struct EventKey
 		{
-			bool oldMountOverlay = DisplayMountOverlay;
+			uint vk : 31;
+			bool down : 1;
+		};
 
-			bool mountOverlay = !Cfg.MountOverlayKeybind().empty() && std::includes(DownKeys.begin(), DownKeys.end(), Cfg.MountOverlayKeybind().begin(), Cfg.MountOverlayKeybind().end());
-			bool mountOverlayLocked = !Cfg.MountOverlayLockedKeybind().empty() && std::includes(DownKeys.begin(), DownKeys.end(), Cfg.MountOverlayLockedKeybind().begin(), Cfg.MountOverlayLockedKeybind().end());
+		std::list<EventKey> eventKeys;
 
-			DisplayMountOverlay = mountOverlayLocked || mountOverlay;
-
-			if (DisplayMountOverlay && !oldMountOverlay)
+		// Generate our EventKey list for the current message
+		{
+			bool eventDown = false;
+			switch (msg)
 			{
-				// Mount overlay is turned on
-
-				if (mountOverlayLocked)
+			case WM_SYSKEYDOWN:
+			case WM_KEYDOWN:
+				eventDown = true;
+			case WM_SYSKEYUP:
+			case WM_KEYUP:
+				if ((msg == WM_SYSKEYDOWN || msg == WM_SYSKEYUP) && wParam != VK_F10)
 				{
-					OverlayPosition.x = OverlayPosition.y = 0.5f;
+					if (((lParam >> 29) & 1) == 1)
+						eventKeys.push_back({ VK_MENU, true });
+					else
+						eventKeys.push_back({ VK_MENU, false });
+				}
 
-					// Attempt to move the cursor to the middle of the screen
-					if (Cfg.ResetCursorOnLockedKeybind())
+				eventKeys.push_back({ (uint)wParam, eventDown });
+				break;
+
+			case WM_LBUTTONDOWN:
+				eventDown = true;
+			case WM_LBUTTONUP:
+				eventKeys.push_back({ VK_LBUTTON, eventDown });
+				break;
+			case WM_MBUTTONDOWN:
+				eventDown = true;
+			case WM_MBUTTONUP:
+				eventKeys.push_back({ VK_MBUTTON, eventDown });
+				break;
+			case WM_RBUTTONDOWN:
+				eventDown = true;
+			case WM_RBUTTONUP:
+				eventKeys.push_back({ VK_RBUTTON, eventDown });
+				break;
+			case WM_XBUTTONDOWN:
+				eventDown = true;
+			case WM_XBUTTONUP:
+				eventKeys.push_back({ (uint)(GET_XBUTTON_WPARAM(wParam) == XBUTTON1 ? VK_XBUTTON1 : VK_XBUTTON2), eventDown });
+				break;
+			}
+		}
+
+		// Apply key events now
+		for (const auto& k : eventKeys)
+			if (k.down)
+				DownKeys.insert(k.vk);
+			else
+				DownKeys.erase(k.vk);
+
+		// Detect hovered section of the radial menu, if visible
+		if (DisplayMountOverlay && msg == WM_MOUSEMOVE)
+			DetermineHoveredMount();
+
+		bool isMenuKeybind = false;
+
+		// Only run these for key down/key up (incl. mouse buttons) events
+		if (!eventKeys.empty())
+		{
+			// Very exclusive test: *only* consider the menu keybind to be activated if they're the *only* keys currently down
+			// This minimizes the likelihood of the menu randomly popping up when it shouldn't
+			isMenuKeybind = DownKeys == Cfg.SettingsKeybind();
+
+			if (isMenuKeybind)
+				DisplayOptionsWindow = true;
+			else
+			{
+				bool oldMountOverlay = DisplayMountOverlay;
+
+				bool mountOverlay = !Cfg.MountOverlayKeybind().empty() && std::includes(DownKeys.begin(), DownKeys.end(), Cfg.MountOverlayKeybind().begin(), Cfg.MountOverlayKeybind().end());
+				bool mountOverlayLocked = !Cfg.MountOverlayLockedKeybind().empty() && std::includes(DownKeys.begin(), DownKeys.end(), Cfg.MountOverlayLockedKeybind().begin(), Cfg.MountOverlayLockedKeybind().end());
+
+				DisplayMountOverlay = mountOverlayLocked || mountOverlay;
+
+				if (DisplayMountOverlay && !oldMountOverlay)
+				{
+					// Mount overlay is turned on
+
+					if (mountOverlayLocked)
 					{
-						RECT rect = { 0 };
-						if (GetWindowRect(GameWindow, &rect))
+						OverlayPosition.x = OverlayPosition.y = 0.5f;
+
+						// Attempt to move the cursor to the middle of the screen
+						if (Cfg.ResetCursorOnLockedKeybind())
 						{
-							if (SetCursorPos((rect.right - rect.left) / 2 + rect.left, (rect.bottom - rect.top) / 2 + rect.top))
+							RECT rect = { 0 };
+							if (GetWindowRect(GameWindow, &rect))
 							{
-								auto& io = ImGui::GetIO();
-								io.MousePos.x = ScreenWidth * 0.5f;
-								io.MousePos.y = ScreenHeight * 0.5f;
+								if (SetCursorPos((rect.right - rect.left) / 2 + rect.left, (rect.bottom - rect.top) / 2 + rect.top))
+								{
+									auto& io = ImGui::GetIO();
+									io.MousePos.x = ScreenWidth * 0.5f;
+									io.MousePos.y = ScreenHeight * 0.5f;
+								}
 							}
 						}
 					}
-				}
-				else
-				{
-					const auto& io = ImGui::GetIO();
-					OverlayPosition.x = io.MousePos.x / (float)ScreenWidth;
-					OverlayPosition.y = io.MousePos.y / (float)ScreenHeight;
-				}
-
-				OverlayTime = timeInMS();
-
-				DetermineHoveredMount();
-			}
-			else if (!DisplayMountOverlay && oldMountOverlay)
-			{
-				// Mount overlay is turned off, send the keybind
-				if (CurrentMountHovered != MountType::NONE)
-					SendKeybind(Cfg.MountKeybind((uint)CurrentMountHovered));
-
-				CurrentMountHovered = MountType::NONE;
-			}
-
-			{
-				// If a key was lifted, we consider the key combination *prior* to this key being lifted as the keybind
-				bool keyLifted = false;
-				auto fullKeybind = DownKeys;
-				for (const auto& ek : eventKeys)
-				{
-					if (!ek.down)
+					else
 					{
-						fullKeybind.insert(ek.vk);
-						keyLifted = true;
+						const auto& io = ImGui::GetIO();
+						OverlayPosition.x = io.MousePos.x / (float)ScreenWidth;
+						OverlayPosition.y = io.MousePos.y / (float)ScreenHeight;
 					}
+
+					OverlayTime = timeInMS();
+
+					DetermineHoveredMount();
+				}
+				else if (!DisplayMountOverlay && oldMountOverlay)
+				{
+					// Mount overlay is turned off, send the keybind
+					if (CurrentMountHovered != MountType::NONE)
+						SendKeybind(Cfg.MountKeybind((uint)CurrentMountHovered));
+
+					CurrentMountHovered = MountType::NONE;
 				}
 
-				// Explicitly filter out M1 (left mouse button) from keybinds since it breaks too many things
-				fullKeybind.erase(VK_LBUTTON);
+				{
+					// If a key was lifted, we consider the key combination *prior* to this key being lifted as the keybind
+					bool keyLifted = false;
+					auto fullKeybind = DownKeys;
+					for (const auto& ek : eventKeys)
+					{
+						if (!ek.down)
+						{
+							fullKeybind.insert(ek.vk);
+							keyLifted = true;
+						}
+					}
 
-				MainKeybind.CheckSetKeybind(fullKeybind, keyLifted);
-				MainLockedKeybind.CheckSetKeybind(fullKeybind, keyLifted);
+					// Explicitly filter out M1 (left mouse button) from keybinds since it breaks too many things
+					fullKeybind.erase(VK_LBUTTON);
 
-				for (uint i = 0; i < MountTypeCount; i++)
-					MountKeybinds[i].CheckSetKeybind(fullKeybind, keyLifted);
+					MainKeybind.CheckSetKeybind(fullKeybind, keyLifted);
+					MainLockedKeybind.CheckSetKeybind(fullKeybind, keyLifted);
+
+					for (uint i = 0; i < MountTypeCount; i++)
+						MountKeybinds[i].CheckSetKeybind(fullKeybind, keyLifted);
+				}
 			}
 		}
-	}
 
 #if 0
-	if (input_key_down || input_key_up)
-	{
-		std::string keybind = "";
-		for (const auto& k : DownKeys)
+		if (input_key_down || input_key_up)
 		{
-			keybind += GetKeyName(k) + std::string(" + ");
+			std::string keybind = "";
+			for (const auto& k : DownKeys)
+			{
+				keybind += GetKeyName(k) + std::string(" + ");
+			}
+			keybind = keybind.substr(0, keybind.size() - 2) + "\n";
+
+			OutputDebugStringA(("Current keys down: " + keybind).c_str());
+
+			char buf[1024];
+			sprintf_s(buf, "msg=%u wParam=%u lParam=%u\n", msg, (uint)wParam, (uint)lParam);
+			OutputDebugStringA(buf);
 		}
-		keybind = keybind.substr(0, keybind.size() - 2) + "\n";
-
-		OutputDebugStringA(("Current keys down: " + keybind).c_str());
-
-		char buf[1024];
-		sprintf_s(buf, "msg=%u wParam=%u lParam=%u\n", msg, (uint)wParam, (uint)lParam);
-		OutputDebugStringA(buf);
-	}
 #endif
 
-	ImGui_ImplDX9_WndProcHandler(hWnd, msg, wParam, lParam);
+		ImGui_ImplDX9_WndProcHandler(hWnd, msg, wParam, lParam);
 
-	// Prevent game from receiving the settings menu keybind
-	if (!eventKeys.empty() && isMenuKeybind)
-		return true;
+		// Prevent game from receiving the settings menu keybind
+		if (!eventKeys.empty() && isMenuKeybind)
+			return true;
 
-	// Prevent game cursor/camera from moving when the overlay is displayed
-	if (DisplayMountOverlay && Cfg.LockCameraWhenOverlayed())
-	{
+		// Prevent game cursor/camera from moving when the overlay is displayed
+		if (DisplayMountOverlay && Cfg.LockCameraWhenOverlayed())
+		{
+			switch (msg)
+			{
+			case WM_MOUSEMOVE:
+				return true;
+			case WM_INPUT:
+			{
+				UINT dwSize = 40;
+				static BYTE lpb[40];
+
+				GetRawInputData((HRAWINPUT)lParam, RID_INPUT,
+					lpb, &dwSize, sizeof(RAWINPUTHEADER));
+
+				RAWINPUT* raw = (RAWINPUT*)lpb;
+
+				if (raw->header.dwType == RIM_TYPEMOUSE)
+					return true;
+
+				break;
+			}
+			case WM_LBUTTONDOWN:
+			case WM_LBUTTONUP:
+			case WM_RBUTTONDOWN:
+			case WM_RBUTTONUP:
+			case WM_MBUTTONDOWN:
+			case WM_MBUTTONUP:
+			case WM_XBUTTONDOWN:
+			case WM_XBUTTONUP:
+			case WM_MOUSEWHEEL:
+			case WM_LBUTTONDBLCLK:
+			case WM_RBUTTONDBLCLK:
+			case WM_MBUTTONDBLCLK:
+			case WM_XBUTTONDBLCLK:
+			{
+				const auto& io2 = ImGui::GetIO();
+
+				short mx, my;
+				mx = (short)io2.MousePos.x;
+				my = (short)io2.MousePos.y;
+				lParam = MAKELPARAM(mx, my);
+				break;
+			}
+			}
+		}
+
+		// Prevent game from receiving input if ImGui requests capture
+		const auto& io = ImGui::GetIO();
 		switch (msg)
 		{
-		case WM_MOUSEMOVE:
-			return true;
-		case WM_INPUT:
-		{
-			UINT dwSize = 40;
-			static BYTE lpb[40];
-
-			GetRawInputData((HRAWINPUT)lParam, RID_INPUT,
-				lpb, &dwSize, sizeof(RAWINPUTHEADER));
-
-			RAWINPUT* raw = (RAWINPUT*)lpb;
-
-			if (raw->header.dwType == RIM_TYPEMOUSE)
-				return true;
-
-			break;
-		}
 		case WM_LBUTTONDOWN:
 		case WM_LBUTTONUP:
 		case WM_RBUTTONDOWN:
@@ -462,49 +499,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		case WM_RBUTTONDBLCLK:
 		case WM_MBUTTONDBLCLK:
 		case WM_XBUTTONDBLCLK:
-		{
-			const auto& io2 = ImGui::GetIO();
-
-			short mx, my;
-			mx = (short)io2.MousePos.x;
-			my = (short)io2.MousePos.y;
-			lParam = MAKELPARAM(mx, my);
+			if (io.WantCaptureMouse)
+				return true;
+			break;
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		case WM_SYSKEYDOWN:
+		case WM_SYSKEYUP:
+			if (io.WantCaptureKeyboard)
+				return true;
+			break;
+		case WM_CHAR:
+			if (io.WantTextInput)
+				return true;
 			break;
 		}
-		}
-	}
-
-	// Prevent game from receiving input if ImGui requests capture
-	const auto& io = ImGui::GetIO();
-	switch (msg)
-	{
-	case WM_LBUTTONDOWN:
-	case WM_LBUTTONUP:
-	case WM_RBUTTONDOWN:
-	case WM_RBUTTONUP:
-	case WM_MBUTTONDOWN:
-	case WM_MBUTTONUP:
-	case WM_XBUTTONDOWN:
-	case WM_XBUTTONUP:
-	case WM_MOUSEWHEEL:
-	case WM_LBUTTONDBLCLK:
-	case WM_RBUTTONDBLCLK:
-	case WM_MBUTTONDBLCLK:
-	case WM_XBUTTONDBLCLK:
-		if (io.WantCaptureMouse)
-			return true;
-		break;
-	case WM_KEYDOWN:
-	case WM_KEYUP:
-	case WM_SYSKEYDOWN:
-	case WM_SYSKEYUP:
-		if (io.WantCaptureKeyboard)
-			return true;
-		break;
-	case WM_CHAR:
-		if (io.WantTextInput)
-			return true;
-		break;
 	}
 
 	// Whatever's left should be sent to the game
