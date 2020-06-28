ba3bd659ee135c3356f7304715b709bd8d233629
megai2 <megai2@ya.ru>
2020-06-29 00:29:51 +0300

    gw2 addon loader support/conversion patch (#129)
    * downgrade to vs2017, disable post build event by default
    
    * addon loader port draft
    
    * fixed present event enable code
    
    * add proper version numbers and depends for addon loader
    
    * Revert "downgrade to vs2017, disable post build event by default"
    
    This reverts commit 08194dc95a38e1a96ad3a5bad020c875fcc25739.
    
    * code cleanup
    
    -moved hooks to proper file
    -added on release cleanups
    -changed target dll name
    
    * Revert "downgrade to vs2017, disable post build event by default"
    
    * fixed gittery instability

--- GW2Radial/include/Direct3D9Hooks.h
+++ GW2Radial/include/Direct3D9Hooks.h
@@ -3,9 +3,9 @@
 #include <Main.h>
 #include <xxhash/xxhash.h>
 #include <d3d9.h>
-#include <Direct3DVirtualFunctionTable.h>
 #include <functional>
 #include <Singleton.h>
+#include "gw2al_api.h"
 
 namespace GW2Radial
 {
@@ -24,16 +24,6 @@ public:
 	
 	Direct3D9Hooks();
 
-	void OnD3DCreate();
-
-	D3DPRESENT_PARAMETERS SetupHookDevice(HWND &hWnd);
-	void DeleteHookDevice(IDirect3DDevice9 *pDev, HWND hWnd);
-
-	void LoadOriginalDevicePointers(IDirect3D9 *d3d);
-	void LoadOriginalDevicePointers(IDirect3D9Ex *d3d);
-
-	IDirect3D9* WINAPI Direct3DCreate9(UINT SDKVersion);
-	HRESULT WINAPI Direct3DCreate9Ex(UINT SDKVersion, IDirect3D9Ex** output);
 
 	const DrawCallback & drawUnderCallback() const { return drawUnderCallback_; }
 	void drawUnderCallback(const DrawCallback &drawUnderCallback) { drawUnderCallback_ = drawUnderCallback; }
@@ -59,72 +49,33 @@ public:
 		postCreateDeviceCallback_ = postCreateDeviceCallback;
 	}
 	
-protected:
-	Present_t Present_real = nullptr;
-	HRESULT WINAPI Present_hook(IDirect3DDevice9 *sThis, CONST RECT *pSourceRect, CONST RECT *pDestRect,
-	                            HWND hDestWindowOverride, CONST RGNDATA *pDirtyRegion);
+	void DevPostRelease(IDirect3DDevice9 *sThis, ULONG refs);
+	void DevPrePresent(IDirect3DDevice9 *sThis);
+	void DevPreReset();
+	void DevPostReset(IDirect3DDevice9 *sThis, D3DPRESENT_PARAMETERS *pPresentationParameters, HRESULT hr);
+	void DevPostCreateVertexShader(const DWORD *pFunction, IDirect3DVertexShader9 **ppShader);
+	void DevPostSetVertexShader(IDirect3DDevice9 *sThis, IDirect3DVertexShader9 *pShader);
+	void DevPostCreatePixelShader(const DWORD *pFunction, IDirect3DPixelShader9 **ppShader);
+	void DevPostSetPixelShader(IDirect3DDevice9 *sThis, IDirect3DPixelShader9 *pShader);
+	void ObjPreCreateDevice(HWND hFocusWindow);
+	void ObjPostCreateDevice(IDirect3DDevice9 *pDevice, D3DPRESENT_PARAMETERS *pPresentationParameters);
 
-	PresentEx_t PresentEx_real = nullptr;
-	HRESULT WINAPI PresentEx_hook(IDirect3DDevice9Ex *sThis, CONST RECT *pSourceRect, CONST RECT *pDestRect,
-	                              HWND hDestWindowOverride, CONST RGNDATA *pDirtyRegion, DWORD dwFlags);
+	void InitHooks(gw2al_core_vtable* gAPI);
 
-	Reset_t Reset_real = nullptr;
-	HRESULT WINAPI Reset_hook(IDirect3DDevice9 *sThis, D3DPRESENT_PARAMETERS *pPresentationParameters);
-
-	ResetEx_t ResetEx_real = nullptr;
-	HRESULT WINAPI ResetEx_hook(IDirect3DDevice9Ex *sThis, D3DPRESENT_PARAMETERS *pPresentationParameters,
-	                            D3DDISPLAYMODEEX *pFullscreenDisplayMode);
-
-	Release_t Release_real = nullptr;
-	ULONG WINAPI Release_hook(IDirect3DDevice9 *sThis);
-
-	AddRef_t AddRef_real = nullptr;
-	ULONG WINAPI AddRef_hook(IDirect3DDevice9 *sThis);
-
-	CreateVertexShader_t CreateVertexShader_real = nullptr;
-	HRESULT WINAPI CreateVertexShader_hook(IDirect3DDevice9 *sThis, const DWORD *pFunction, IDirect3DVertexShader9 **ppShader);
-
-	SetVertexShader_t SetVertexShader_real = nullptr;
-	HRESULT WINAPI SetVertexShader_hook(IDirect3DDevice9 *sThis, IDirect3DVertexShader9 *pShader);
-
-	CreatePixelShader_t CreatePixelShader_real = nullptr;
-	HRESULT WINAPI CreatePixelShader_hook(IDirect3DDevice9 *sThis, const DWORD *pFunction, IDirect3DPixelShader9 **ppShader);
-
-	SetPixelShader_t SetPixelShader_real = nullptr;
-	HRESULT WINAPI SetPixelShader_hook(IDirect3DDevice9 *sThis, IDirect3DPixelShader9 *pShader);
-
-	CreateDevice_t CreateDevice_real = nullptr;
-	HRESULT WINAPI CreateDevice_hook(IDirect3D9 *sThis, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow,
-	                                 DWORD BehaviorFlags, D3DPRESENT_PARAMETERS *pPresentationParameters,
-	                                 IDirect3DDevice9 **ppReturnedDeviceInterface);
-
-	CreateDeviceEx_t CreateDeviceEx_real = nullptr;
-	HRESULT WINAPI CreateDeviceEx_hook(IDirect3D9Ex *sThis, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow,
-	                                   DWORD BehaviorFlags, D3DPRESENT_PARAMETERS *pPresentationParameters,
-	                                   D3DDISPLAYMODEEX *pFullscreenDisplayMode,
-	                                   IDirect3DDevice9 **ppReturnedDeviceInterface);
-	
+protected:	
 	const XXH64_hash_t preUiVertexShaderHash_ = 0x1fe3c6cd77e6e9f0;
 	const XXH64_hash_t preUiPixelShaderHash_ = 0xccc38027cdd6cd51;
 	IDirect3DVertexShader9* preUiVertexShader_ = nullptr;
 	IDirect3DPixelShader9* preUiPixelShader_ = nullptr;
 
-	HMODULE realD3D9Module_ = nullptr;
-	HMODULE chainD3D9Module_ = nullptr;
-	Direct3DDevice9VirtualFunctionTable_t originalDeviceVfTable_ = { nullptr };
-
 	bool isFrameDrawn_ = false;
-	bool isInShaderHook_ = false;
-	bool isDirect3DHooked_ = false;
+	bool isInShaderHook_ = false;	
 
 	DrawCallback drawUnderCallback_, drawOverCallback_;
 	PreResetCallback preResetCallback_;
 	PostResetCallback postResetCallback_;
 	PreCreateDeviceCallback preCreateDeviceCallback_;
 	PostCreateDeviceCallback postCreateDeviceCallback_;
-	
-	Direct3D9VirtualFunctionTable_t direct3D9VirtualFunctionTable_ = { };
-	Direct3DDevice9VirtualFunctionTable_t direct3DDevice9VirtualFunctionTable_ = { };
 };
 
 }
--- GW2Radial/src/Main.cpp
+++ GW2Radial/src/Main.cpp
@@ -1,15 +1,51 @@
 #include <Main.h>
 #include <Core.h>
 #include <Direct3D9Hooks.h>
+#include "gw2al_api.h"
 
-IDirect3D9* WINAPI Direct3DCreate9(UINT SDKVersion)
+gw2al_addon_dsc gAddonDeps[] = {
+	{
+		L"loader_core",
+		L"whatever",
+		0,
+		1,
+		1,
+		0
+	},
+	{
+		L"d3d9_wrapper",
+		L"Wrapper for d3d9 API that includes hooking and custom d3d9 loading",
+		1,
+		0,
+		2,
+		0
+	},
+	{0,0,0,0,0,0}
+};
+
+gw2al_addon_dsc gAddonDsc = {
+	L"gw2radial",
+	L"Radial menu overlay to select mount, novelty and more on fly",
+	1,
+	2,
+	1,
+	gAddonDeps
+};
+
+gw2al_addon_dsc* gw2addon_get_description()
 {
-	return GW2Radial::Direct3D9Hooks::i()->Direct3DCreate9(SDKVersion);
+	return &gAddonDsc;
 }
 
-HRESULT WINAPI Direct3DCreate9Ex(UINT SDKVersion, IDirect3D9Ex** d3d9ex)
+gw2al_api_ret gw2addon_load(gw2al_core_vtable* core_api)
 {
-	return GW2Radial::Direct3D9Hooks::i()->Direct3DCreate9Ex(SDKVersion, d3d9ex);
+	GW2Radial::Direct3D9Hooks::i()->InitHooks(core_api);
+	return GW2AL_OK;
+}
+
+gw2al_api_ret gw2addon_unload(int gameExiting)
+{
+	return GW2AL_OK;
 }
 
 bool WINAPI DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved)
