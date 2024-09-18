596fb629dd8745c209b721f9ded1452b0365d77b
FriendlyFire <jpguertin.cg@gmail.com>
2024-09-17 21:37:00 -0400

    Fixed issues

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,27 +1,31 @@
 #include <Core.h>
 #include <Direct3D11Loader.h>
 #include <Main.h>
-#include <Tag.h>
 #include <Version.h>
 #include <gw2al_api.h>
 #include <gw2al_d3d9_wrapper.h>
 
 const char* GetAddonName()
 {
-    return "GW2Radial";
+    return ADDON_NAME;
 }
+
 const wchar_t* GetAddonNameW()
 {
-    return L"GW2Radial";
+    return TEXT(ADDON_NAME);
 }
+
 const char* GetAddonVersionString()
 {
-    return GW2RADIAL_VER;
+    return GIT_VER_STR;
 }
-const semver::version& GetAddonVersion()
+
+u64 GetAddonVersion()
 {
-    return CurrentVersion;
+    static std::array<u64, 4> ver = { GIT_VER };
+    return (ver[0] << 16 * 3) + (ver[1] << 16 * 2) + (ver[2] << 16 * 1) + (ver[3] << 16 * 0);
 }
+
 BaseCore& GetBaseCore()
 {
     return GW2Radial::Core::i();
@@ -31,11 +35,19 @@ gw2al_addon_dsc gAddonDeps[] = {
     GW2AL_CORE_DEP_ENTRY, D3D_WRAPPER_DEP_ENTRY, {0, 0, 0, 0, 0, 0}
 };
 
-gw2al_addon_dsc  gAddonDsc = { L"gw2radial", L"Radial menu overlay to select mount, novelty and more on fly", 2, 2, 1, gAddonDeps };
 
 gw2al_addon_dsc* gw2addon_get_description()
 {
-    return &gAddonDsc;
+    static gw2al_addon_dsc addonDesc = []
+    {
+        std::array<u64, 4> ver = { GIT_VER };
+        gw2al_addon_dsc    d{ L"gw2radial", L"Radial menu overlay to select mount, novelty and more on fly", 0, 0, 0, gAddonDeps };
+        d.majorVer = (unsigned char)ver[0];
+        d.minorVer = (unsigned char)ver[1];
+        d.revision = (unsigned int)ver[2];
+        return d;
+    }();
+    return &addonDesc;
 }
 
 gw2al_api_ret gw2addon_load(gw2al_core_vtable* core_api)
