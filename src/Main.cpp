a7777afef985daca49ea2c4b419b82c1a1bcd535
jpg <jpguertin.cg@gmail.com>
2025-11-11 01:03:02 -0500

    Updated API and Common

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,3 +8,8 @@ BaseCore& GetBaseCore()
 }
 
 #include <Main.inl>
+
+const cpp_util::cstring_view  AddonName          = "GW2Radial";
+const cpp_util::wcstring_view AddonNameW         = L"GW2Radial";
+const cpp_util::cstring_view  AddonVersionString = GIT_VER_STR;
+const u64                     AddonVersion       = GetAddonVersion();
